8be762d5ab5d02dc682100b8433dfee4c0b233d3
Sue Burt <sookburt@gmail.com>
2022-05-05 18:19:38 +0100

    calling StudioGhibli and getting films, display list in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Film } from './IFilm';
 
 function App() {
+
+  const [ filmState, setFilmState ] = useState<Array<Film>>([]);
+
+  useEffect(() => {
+    getFilms();
+  }, []);
+
+  const getFilms = async() => {
+
+    try {
+      const apiResponse = await axios.get('https://ghibliapi.herokuapp.com/films/');
+      setFilmState(apiResponse.data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+  }
+
   return (
     <div className="App">
       <h1>Welcome to Testing API Calls with MSW</h1>
-
+      {
+        filmState && filmState.map(film => <p>{film.title}</p>)
+      }
     </div>
   );
 }
